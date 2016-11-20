22282bdc7aa5b3b7599bc00ce07d4bc6e668b091
Dennis Iversen <dennis.iversen@gmail.com>
2016-11-20 12:37:31 +0100

    use redbean autoload

--- module.php
+++ module.php
@@ -2,20 +2,20 @@
 
 namespace modules\count;
 
-use diversen\db\rb;
+use RedBeanPHP\R;
 
 class module {
     
     public function increment ($table, $column, $id) {
         
-        $bean = \R::findOne($table,' id = ? ',array($id));
+        $bean = R::findOne($table,' id = ? ',array($id));
         if (!$bean) {
             
             return;
         }
-        \R::begin();
+        R::begin();
         $bean->{$column}++;
-        \R::store($bean);
-        \R::commit();
+        R::store($bean);
+        R::commit();
     }
 }
